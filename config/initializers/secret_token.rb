2f6dd2f73295860e0637d3dae3a1d4031f5a36c3
SBRKS <sbrks3@gmail.com>
2016-07-12 12:09:50 -0700

    rails api

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,19 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+
+# Although this is not needed for an api-only application, rails4 
+# requires secret_key_base or secret_token to be defined, otherwise an 
+# error is raised.
+# Using secret_token for rails3 compatibility. Change to secret_key_base
+# to avoid deprecation warning.
+# Can be safely removed in a rails3 api-only application.
+CardsApi::Application.config.secret_token = '5f1c93bbe73de99642acb39b12a08697530519f78c1ee16fa0af9463614e54ce9a29ed744c6a68182131aeca4eac3a26ed033bd993d17541ba965fad346bfa51'
